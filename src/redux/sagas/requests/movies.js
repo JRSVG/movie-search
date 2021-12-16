7c5740533ddeb6530eb3b5dcbc94f1484ac5b901
unknown <jari.grejtak@gmail.com>
2021-12-16 13:00:14 +0100

    Deployment on Heroku unsuccessful because of insecure requests

--- src/redux/sagas/requests/movies.js
+++ src/redux/sagas/requests/movies.js
@@ -1,18 +1,16 @@
-const REACT_APP_OMDB_API_KEY = '93d91712';
-
 export function requestMoviesByQuery(payload) {
   const { query, page } = payload;
-  const url = `http://omdbapi.com/?apikey=${REACT_APP_OMDB_API_KEY}&s=${query}&page=${page}&r=json`;
+  const url = `https://omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&s=${query}&page=${page}&r=json`;
   console.log('request to: ', url);
   return fetch(url);
 }
 
 export function requestMovieDetailById(id) {
-  const url = `http://omdbapi.com/?apikey=${REACT_APP_OMDB_API_KEY}&i=${id}&r=json`;
+  const url = `https://omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&i=${id}&r=json`;
   return fetch(url);
 }
 
 export function requestMovieById(id) {
-  const url = `http://omdbapi.com/?apikey=${REACT_APP_OMDB_API_KEY}&i=${id}&plot=short&r=json`;
+  const url = `https://omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_API_KEY}&i=${id}&plot=short&r=json`;
   return fetch(url);
 }
